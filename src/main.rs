8d816b384d6a41773148a134ddd18b7b5bf3656f
Tomasz Kawalec <tkawalec369@gmail.com>
2024-11-13 20:00:17 +0100

    Instantiate the blimp's main algorithm, execute it and periodically ping it

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
-use blimp_onboard_software;
 use fixed;
 use nalgebra;
+use postcard;
 use simba;
 use tokio;
 use typenum;
+
+use blimp_onboard_software;
+use blimp_onboard_software::obsw_interface::BlimpAlgorithm;
 
 struct SimBlimp {
     coord_mat: nalgebra::Affine3<f64>,
@@ -17,10 +20,24 @@ struct Simulation {
 
 impl Simulation {
     fn new() -> Self {
+        let mut blimp_main_algo = blimp_onboard_software::obsw_algo::BlimpMainAlgo::new();
+        blimp_main_algo.set_action_callback(Box::new(|action| {
+            // println!("Action {:#?}", action);
+            match action {
+                blimp_onboard_software::obsw_algo::BlimpAction::SendMsg(msg) => {
+                    if let Ok(msg_des) =
+                        postcard::from_bytes::<blimp_onboard_software::obsw_algo::MessageB2G>(&msg)
+                    {
+                        println!("Got message:\n{:#?}", msg_des);
+                    }
+                }
+                _ => {}
+            }
+        }));
         Self {
             blimp: SimBlimp {
                 coord_mat: nalgebra::Affine3::identity(),
-                main_algo: blimp_onboard_software::obsw_algo::BlimpMainAlgo::new(),
+                main_algo: blimp_main_algo,
             },
             earth_radius: 6371000.0,
         }
@@ -33,21 +50,55 @@ impl Simulation {
 
 #[tokio::main]
 async fn main() {
-    let mut sim = Simulation::new();
+    let mut sim = std::sync::Arc::new(tokio::sync::Mutex::new(Simulation::new()));
 
     let (shutdown_tx, mut shutdown_rx) = tokio::sync::broadcast::channel::<()>(1);
 
     {
+        // Execute blimp's algorithm steps
+
         let mut shutdown_rx = shutdown_tx.subscribe();
+        let sim = sim.clone();
         tokio::spawn(async move {
             loop {
-                sim.step().await;
+                sim.lock().await.step().await;
 
                 tokio::select! {
                     _ = tokio::time::sleep(tokio::time::Duration::from_millis(100)) => {},
                     _ = shutdown_rx.recv() => {
                         break;
                     },
+                };
+            }
+        });
+    }
+
+    {
+        // Ping the blimp
+
+        let mut shutdown_rx = shutdown_tx.subscribe();
+        tokio::spawn(async move {
+            let mut i: u32 = 0;
+            loop {
+                println!("Pinging the blimp with id {}", i);
+                sim.lock()
+                    .await
+                    .blimp
+                    .main_algo
+                    .handle_event(&blimp_onboard_software::obsw_algo::BlimpEvent::GetMsg(
+                        postcard::to_stdvec::<blimp_onboard_software::obsw_algo::MessageG2B>(
+                            &blimp_onboard_software::obsw_algo::MessageG2B::Ping(i),
+                        )
+                        .unwrap(),
+                    ))
+                    .await;
+                i += 1;
+
+                tokio::select! {
+                    _ = tokio::time::sleep(tokio::time::Duration::from_millis(1000))=>{}
+                    _ = shutdown_rx.recv()=>{
+                        break;
+                    }
                 };
             }
         });
